51696782e3fb4f16925b6906532f43124ba941f8
kwanwoo <kwanwoo.kim@gmail.com>
2024-08-01 05:07:52 +0900

    golden,death cross 추가

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -7,5 +7,26 @@ class MyTestCase(unittest.TestCase):
         df['MA20']=indicator.ma(df['Close'],20)
         display.show_stock_data(df,['Close','MA20'],'moving average')
 
+    def test_golden_death(self):
+        ticker = 'NVDA'
+        df = data.get_year_data(ticker)
+        df['MA05'] = indicator.ma(df['Close'], 5)
+        df['MA25'] = indicator.ma(df['Close'], 25)
+        indi=indicator.golden_death_cross(df['MA05'],df['MA25'])
+        df['Golden'] = indi['Golden']
+        df['Death'] = indi['Death']
+        display.show_golden_death_cross(df,title=ticker )
+
+    def test_vol_golden_death(self):
+        ticker = 'NVDA'
+        df = data.get_year_data(ticker)
+        df['VOL05'] = indicator.ma(df['Volume'], 5)
+        df['VOL25'] = indicator.ma(df['Volume'], 25)
+        indi=indicator.golden_death_cross(df['VOL05'],df['VOL25'])
+        df['Golden'] = indi['Golden']
+        df['Death'] = indi['Death']
+        display.show_vol_golden_death_cross(df,title=ticker )
+
+
 if __name__ == '__main__':
     unittest.main()
--- util/display.py
+++ util/display.py
@@ -9,3 +9,42 @@ def show_stock_data(df, colnames, title='stock data'):
     plt.legend()
     plt.grid(True)
     plt.show()
+
+def show_golden_death_cross(df, title='golden/death cross'):
+    plt.figure(figsize=(14, 7))
+    golden = df[df['Golden']]
+    death = df[df['Death']]
+
+    #plt.plot(df['MA05'], color='r', label='MA05')
+    #plt.plot(df['MA25'], color='b', label='MA25')
+    plt.plot(df['Close'], color='k',label='Close')
+    plt.plot(golden['Close'], '^', markersize=10, color='r', label='Golden Cross')
+    plt.plot(death['Close'], 'v', markersize=10, color='b', label='Death Cross')
+    plt.title(title)
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def show_vol_golden_death_cross(df, title='golden/death cross'):
+    fig, ax1 = plt.subplots(figsize=(14, 7))
+    golden = df[df['Golden']]
+    death = df[df['Death']]
+
+    #ax1.plot(df['VOL05'], color='r', label='VOL05')
+    #ax1.plot(df['VOL25'], color='b', label='VOL25')
+    ax1.legend(loc = 'upper left')
+    ax1.set_ylabel('Volume')
+
+    ax2 = ax1.twinx()
+    ax2.plot(df['Close'], color='k',label='Close')
+    ax2.plot(golden['Close'], '^', markersize=10, color='r', label='Golden Cross')
+    ax2.plot(death['Close'], 'v', markersize=10, color='b', label='Death Cross')
+    ax2.legend(loc='upper right')
+    ax2.set_ylabel('Price')
+
+    plt.title(title)
+    plt.xlabel('Date')
+    plt.grid(True)
+    plt.show()
