5c445d64a958a422c70d1fbf48d5455814a10433
kwanwoo <kwanwoo.kim@gmail.com>
2024-08-03 22:10:59 +0900

    리팩토링

--- tests/test_indicator.py
+++ tests/test_indicator.py
@@ -11,24 +11,38 @@ class MyTestCase(unittest.TestCase):
         window = 5
         ma_col = f'MA{self.col}{window}'
         df = indicator.ma(df,window)
-        df = df[[self.col,ma_col]]
-        display.show_stock_data(df, title='moving average')
+        plot = display.Plot()
+        plot.set_stock_data(df[['Close','MAClose5']])
+        plot.show('moving average')
+        print(df)
 
     def test_close_golden_death(self):
         df = self.df.copy()
         target = 'Close'
         df=indicator.golden_death_cross(df,target=target, short=5, long=25)
-        display.show_golden_death_cross(df,title=f'{self.ticker}_{target}')
+        plot = display.Plot()
+        slice_df = df.loc['2024-07-01':'2024-08-05']
+        plot.set_golden_death_cross(slice_df)
+        plot.show('Close Golden/Death')
+        print(df)
 
     def test_vol_golden_death(self):
         df = self.df.copy()
         target = 'Volume'
         df=indicator.golden_death_cross(df,target='Volume', short=5, long=25)
-        display.show_golden_death_cross(df,title=f'{self.ticker}_{target}')
+        plot = display.Plot()
+        plot.set_vol_golden_death_cross(df)
+        plot.show('Vol Golden/Death')
+        print(df)
 
     def test_ilmok(self):
         df = self.df.copy()
         df = indicator.ichimoku(df)
+        slice_df = df.loc['2024-07-01':'2024-08-05']
+        plot = display.Plot()
+        plot.set_ichimoku(slice_df)
+        plot.set_close(slice_df)
+        plot.show('일목요연표')
         print(df)
 
 if __name__ == '__main__':
--- util/display.py
+++ util/display.py
@@ -1,50 +1,74 @@
 import matplotlib.pyplot as plt
-def show_stock_data(df, title='stock data'):
-    plt.figure(figsize=(14, 7))
-    for col in df.columns:
-        plt.plot(df[col], label=col)
-    plt.title(title)
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+import matplotlib.dates as mdates
 
-def show_golden_death_cross(df, title='golden/death cross'):
-    plt.figure(figsize=(14, 7))
-    golden = df[df['Golden']]
-    death = df[df['Death']]
+# 한글 폰트 경로 설정 (예: 나눔고딕)
+plt.rcParams['font.family'] = 'NanumGothic'
+plt.rcParams['axes.unicode_minus'] = False
 
-    #plt.plot(df['MA05'], color='r', label='MA05')
-    #plt.plot(df['MA25'], color='b', label='MA25')
-    plt.plot(df['Close'], color='k',label='Close')
-    plt.plot(golden['Close'], '^', markersize=10, color='r', label='Golden Cross')
-    plt.plot(death['Close'], 'v', markersize=10, color='b', label='Death Cross')
-    plt.title(title)
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+class Plot:
+    def __init__(self):
+        self.fig, self.ax = plt.subplots(figsize=(14, 7))
 
-def show_vol_golden_death_cross(df, title='golden/death cross'):
-    fig, ax1 = plt.subplots(figsize=(14, 7))
-    golden = df[df['Golden']]
-    death = df[df['Death']]
+    def show(self, title='plot'):
+        self.ax.set_title(title)
+        self.ax.set_xlabel('Date')
+        self.ax.set_ylabel('Price')
+        self.ax.legend()
+        # x축 설정
+        self.ax.xaxis.set_major_locator(mdates.DayLocator())  # 주 눈금을 일 단위로 설정
+        self.ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))  # 날짜 형식 설정
 
-    #ax1.plot(df['VOL05'], color='r', label='VOL05')
-    #ax1.plot(df['VOL25'], color='b', label='VOL25')
-    ax1.legend(loc = 'upper left')
-    ax1.set_ylabel('Volume')
+        # 그리드 설정
+        self.ax.grid(True, which='major', axis='x', linestyle='--', color='gray', alpha=0.7)
 
-    ax2 = ax1.twinx()
-    ax2.plot(df['Close'], color='k',label='Close')
-    ax2.plot(golden['Close'], '^', markersize=10, color='r', label='Golden Cross')
-    ax2.plot(death['Close'], 'v', markersize=10, color='b', label='Death Cross')
-    ax2.legend(loc='upper right')
-    ax2.set_ylabel('Price')
+        self.ax.grid(True)
 
-    plt.title(title)
-    plt.xlabel('Date')
-    plt.grid(True)
-    plt.show()
+        # x축 레이블 회전
+        plt.xticks(rotation=45, ha='right')
+
+        # 레이아웃 조정
+        plt.tight_layout()
+        self.fig.show()
+
+    def set_stock_data(self, df):
+        for col in df.columns:
+            self.ax.plot(df[col], label=col)
+
+    def set_golden_death_cross(self, df):
+        golden = df[df['Golden']]
+        death = df[df['Death']]
+        self.ax.plot(df['Close'], color='k',label='Close')
+        self.ax.plot(golden['Close'], '^', markersize=10, color='r', label='Golden Cross')
+        self.ax.plot(death['Close'], 'v', markersize=10, color='b', label='Death Cross')
+
+    def set_vol_golden_death_cross(self,df):
+        golden = df[df['Golden']]
+        death = df[df['Death']]
+        self.ax.plot(df['Close'], color='k',label='Close')
+        self.ax.plot(golden['Close'], '^', markersize=10, color='r', label='Golden Cross')
+        self.ax.plot(death['Close'], 'v', markersize=10, color='b', label='Death Cross')
+        self.ax.legend(loc = 'upper left')
+
+        ax2 = self.ax.twinx()
+        ax2.plot(df['Volume'], color='blue',label='Volume')
+        ax2.legend(loc='upper right')
+
+
+    def set_ichimoku(self, df, title="일목균형표"):
+        # plt.plot(df.index, df['Close'],linewidth=1, linestyle='-.', label='종가', color='black')
+        self.ax.plot(df.index, df['conversion_line'], label='전환선', color='gray')
+        self.ax.plot(df.index, df['base_line'], label='기준선', color='purple')
+        self.ax.plot(df.index, df['leading_span_a'], label='선행스팬1', color='red')
+        self.ax.plot(df.index, df['leading_span_b'], label='선행스팬2', color='blue')
+        self.ax.plot(df.index, df['lagging_span'], label='후행스팬', color='orange')
+
+        self.ax.fill_between(df.index, df['leading_span_a'], df['leading_span_b'],
+                         where=df['leading_span_a'] >= df['leading_span_b'], facecolor='red', alpha=0.5)
+        self.ax.fill_between(df.index, df['leading_span_a'], df['leading_span_b'],
+                         where=df['leading_span_a'] < df['leading_span_b'], facecolor='blue', alpha=0.5)
+
+
+
+    def set_close(self,df):
+        self.ax.plot(df.index, df['Close'],linewidth=1, linestyle='-.', label='종가', color='black')
+
