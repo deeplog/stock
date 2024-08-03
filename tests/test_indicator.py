04ee3d0f50753d72571c4901f2b86b81a68057b3
kwanwoo <kwanwoo.kim@gmail.com>
2024-08-04 05:53:41 +0900

    리팩토링

--- tests/test_indicator.py
+++ tests/test_indicator.py
@@ -14,15 +14,14 @@ class MyTestCase(unittest.TestCase):
         df = df[['Close', 'MAClose5']]
         plot = display.Plot(df, '2024-07-01', '2024-08-05')
         plot.stock_data('moving average')
-        print(df)
+        plot.fig.savefig('test_output/ma.png')
 
     def test_close_golden_death(self):
         df = self.df.copy()
         df=indicator.golden_death_cross(df,target='Close', short=5, long=25)
         plot = display.Plot(df, '2024-07-01', '2024-08-05')
         plot.golden_death_cross()
-        plot.show('Close Golden/Death')
-        print(df)
+        plot.fig.savefig('test_output/close_golden_death.png')
 
     def test_vol_golden_death(self):
         df = self.df.copy()
@@ -30,14 +29,14 @@ class MyTestCase(unittest.TestCase):
         df=indicator.golden_death_cross(df,target='Volume', short=5, long=25)
         plot = display.Plot(df, '2024-07-01', '2024-08-05')
         plot.vol_golden_death_cross('volume golden cross')
-        print(df)
+        plot.fig.savefig('test_output/vol_golden_death.png')
 
     def test_ilmok(self):
         df = self.df.copy()
         df = indicator.ichimoku(df)
         plot = display.Plot(df, '2024-07-01', '2024-08-05')
         plot.ichimoku()
-        print(df)
+        plot.fig.savefig('test_output/ilmok.png')
 
     def test_counterclock(self):
         df = self.df.copy()
@@ -45,7 +44,7 @@ class MyTestCase(unittest.TestCase):
         df['Price']=df['MAClose20']
         plot = display.Plot(df, '2024-05-01', '2024-08-05')
         plot.counterclock(title='20일 이동평균 역시계곡선')
-        print(df)
+        plot.fig.savefig('test_output/역시계곡선.png')
 
 
 if __name__ == '__main__':
